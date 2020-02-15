029d9d206ab25a1797a6484ea8447c183ec7d934
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-02-15 12:17:18 -0300

    add Header

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Header.css';
+import Logo from '../assets/logo-facebook.svg';
+import User from '../assets/user.svg';
+
+function Header() {
+  return (
+    <header>
+      <img class="logo" src={Logo} alt="logo" />
+      <ul>
+        <li><p>Meu perfil</p></li>
+        <li><img src={User} /></li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header;
